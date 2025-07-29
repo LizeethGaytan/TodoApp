de6f77e3904c009de6af101dcfe87d3973d2ea2c
LizeethGaytan <52652399+LizeethGaytan@users.noreply.github.com>
2025-07-28 19:39:12 -0700

    Added TodoListItem file + updated TodoList and App files

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
+import { useState } from 'react';
 import './App.css';
-{
-  /*imported TodoList component*/
-}
-import './TodoList';
+// Imported components
 import TodoList from './TodoList';
-{
-  /*imported TodoForm component*/
-}
-import './TodoForm';
 import TodoForm from './TodoForm';
 
 function App() {
+  // state value that holds a new todo
+  const [newTodo, setNewTodo] = useState('Todo?');
   return (
     <div>
       <h1>TO-DOS</h1>
       {/*Added an instance of TodoForm*/}
       <TodoForm />
+      <p>{newTodo}</p>
       {/*Added an instance of the TodoList*/}
       <TodoList />
     </div>
