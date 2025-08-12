a96adabee822160e74a395e2bac2be64c7553dbd
LizeethGaytan <52652399+LizeethGaytan@users.noreply.github.com>
2025-08-11 21:31:11 -0700

    Updates to TodoList, App, and TodoForm files

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,25 +1,34 @@
 import { useRef } from 'react';
+import { useState } from 'react';
 
 // TodoFrom component, returns a form
 // Self Note: Invoking onAddTodo needs to be passed in as a prop could be done as (props) then called props.onAddTodo() or {onAddTodo} and called onAddTodo()?
 function TodoForm({ onAddTodo }) {
   // useRef invoked with an empty string
   const todoTitleInput = useRef();
+  //use state
+  const [workingTodoTitle, setWorkingTodoTitle] = useState('');
   // Function takes event object
   function handleAddTodo(event) {
     event.preventDefault();
-    const title = event.target.title.value;
-    onAddTodo(title);
-
-    event.target.title.value = '';
+    onAddTodo(workingTodoTitle);
+    setWorkingTodoTitle('');
     todoTitleInput.current.focus();
   }
   return (
-    // Added onSubmit event handler and passed handleAddTodo function
+    // Added onSubmit event handler/listener and passed handleAddTodo function
     <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Todo:</label>
-      <input id="todoTitle" name="title" ref={todoTitleInput}></input>
-      <button type="submit">Add Todo</button>
+      <input
+        id="todoTitle"
+        name="title"
+        value={workingTodoTitle}
+        ref={todoTitleInput}
+        onChange={(e) => setWorkingTodoTitle(e.target.value)}
+      ></input>
+      <button type="submit" disabled={workingTodoTitle === ''}>
+        Add Todo
+      </button>
     </form>
   );
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,10 +1,20 @@
 import TodoListItem from './TodoListItem';
 //Created a basic structure for the TodoList component(basically a function)
-function TodoList({ todoList }) {
-  return (
+function TodoList({ todoList, onCompleteTodo }) {
+  // below variable contains an array of todo items that have not been completed/checked off
+  const filteredTodoList = todoList.filter((todo) => !todo.isCompleted);
+  return filteredTodoList.length === 0 ? (
+    <p>"Get started! Add a todo above"</p>
+  ) : (
     <ul>
-      {todoList.map((todo) => (
-        <TodoListItem key={todo.id} todo={todo} />
+      {/* change todoList references with filteredTodoList */}
+      {filteredTodoList.map((todo) => (
+        <TodoListItem
+          key={todo.id}
+          todo={todo}
+          todoList={todoList}
+          onCompleteTodo={onCompleteTodo}
+        />
       ))}
     </ul>
   );
