495f9d779b77304ce7d005ca8e234fc521017b11
LizeethGaytan <52652399+LizeethGaytan@users.noreply.github.com>
2025-08-19 10:24:13 -0700

    Reusable components, Project Organization, Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import './App.css';
 // Imported components
-import TodoList from './features/TodoList';
-import TodoForm from './TodoForm';
+import TodoList from './features/TodoList/TodoList';
+import TodoForm from './features/TodoForm';
 
 function App() {
   // useState: state value/variable(todoList), an update function(setTodoList), and a hook initialized with an empty array
@@ -24,13 +24,28 @@ function App() {
     const newTodo = { id: Date.now(), title, isCompleted: false };
     setTodoList([...todoList, newTodo]);
   }
+  function updateTodo(editedTodo) {
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === editedTodo.id) {
+        // this area might be wrong, still a bit confused with destructuring(not sure if it should be of concern given I'm 6 weeks into the course)
+        // const { ...editedTodo } = updatedTodo;
+        return { ...editedTodo };
+      }
+      return todo;
+    });
+    setTodoList(updatedTodos);
+  }
   return (
     <div>
       <h1>TODOS</h1>
       {/*TodoForm instance with an onAddTodo prop that's passes a handler function, addTodo*/}
       <TodoForm onAddTodo={addTodo} />
       {/*TodoList instance with a todoList state value and completeTodo helper function passed in as props*/}
-      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
+      <TodoList
+        todoList={todoList}
+        onCompleteTodo={completeTodo}
+        onUpdateTodo={updateTodo}
+      />
     </div>
   );
 }
---
+++ src/features/TodoList/TodoListItem.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import TextInputWithLabel from '../../shared/TextInputWithLabel';
+function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [workingTitle, setWorkingTitle] = useState(todo.title);
+  // Event helper function, there's a difference b/w an event helper and handler function though both handle user interactions
+  function handleCancel() {
+    workingTitle;
+    setIsEditing(false);
+  }
+  function handleEdit(event) {
+    setWorkingTitle(event.target.value);
+  }
+  function handleUpdate(event) {
+    if (!isEditing) {
+      return;
+    }
+    event.preventDefault();
+    // the below destructuring kind of helped me have a better understanding of destructuring
+    onUpdateTodo({ ...todo, title: workingTitle });
+    setIsEditing(false);
+  }
+  return (
+    <li>
+      <form onSubmit={handleUpdate}>
+        {isEditing ? (
+          <>
+            <TextInputWithLabel value={workingTitle} onChange={handleEdit} />
+            <button type="button" onClick={handleCancel}>
+              Cancel
+            </button>
+            <button type="button" onClick={handleUpdate}>
+              Update
+            </button>
+          </>
+        ) : (
+          <>
+            <label>
+              {' '}
+              <input
+                type="checkbox"
+                checked={todo.isCompleted}
+                onChange={() => onCompleteTodo(todo.id)}
+              />
+            </label>
+            <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+          </>
+        )}
+      </form>
+    </li>
+  );
+}
+
+export default TodoListItem;
